1d9941557164796db1438d4519cbdf83e53d84e6
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2023-01-23 23:11:34 -0300

    splash screen

---
+++ app/src/main/java/com/example/weatherforecast/SplashActivity.kt
@@ -0,0 +1,29 @@
+package com.example.weatherforecast;
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.weatherforecast.MainActivity
+import com.example.weatherforecast.R
+
+
+class SplashActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+        window.statusBarColor = Color.parseColor("#006600")
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+
+}
